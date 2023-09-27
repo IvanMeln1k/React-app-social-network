854c7a9f7735195913327a725d468b45c5d537d7
Ivan Melnik <IvanMelnikV@ya.ru>
2023-09-27 22:27:01 +0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Header = () => {
+  return (
+    <header>
+      <div>
+        <a href="#">
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4Q4hcU4RO5X6uAYlqB6X-kit4tOIAmqYG7gJHZFg5gIyFcRbgGRN5fZt5JeGuBdgn52I&usqp=CAU" alt="logo social network" />
+        </a>
+      </div>
+    </header>
+  )
+}
