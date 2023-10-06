af1efef74c5130844e482c76ff76029fbbb97000
Ivan Melnik <IvanMelnikV@ya.ru>
2023-10-06 10:44:08 +0300

    Class component users

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -3,24 +3,33 @@ import User from "./User/User";
 import s from "./Users.module.scss";
 import axios from "axios";
 
-const Users = (props) => {
-  const getUsers = () => {
+class Users extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
     axios
       .get("https://social-network.samuraijs.com/api/1.0/users?count=10&page=1")
-      .then((response) => props.setUsers(response.data.items));
-  };
+      .then((response) => this.props.setUsers(response.data.items));
+  }
 
-  return (
-    <div className={s.wrapper}>
-      <h2 className={s.title}>Users</h2>
-      <button onClick={getUsers}>Get users</button>
-      <div className={s.users}>
-        {props.users.map((user) => (
-          <User follow={props.follow} unfollow={props.unfollow} user={user} />
-        ))}
+  render() {
+    return (
+      <div className={s.wrapper}>
+        <h2 className={s.title}>Users</h2>
+        <div className={s.users}>
+          {this.props.users.map((user) => (
+            <User
+              follow={this.props.follow}
+              unfollow={this.props.unfollow}
+              user={user}
+            />
+          ))}
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Users;
